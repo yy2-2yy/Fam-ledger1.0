e1e157be3e8bded3bd030fd9bd79416a16f2c6ff
3 <53806818+yy2-2yy@users.noreply.github.com>
2026-07-07 13:27:20 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "family-ledger-v4";
+const CACHE = "family-ledger-v5";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
